9a31a6d51c04a910fd70932e08be2dbc8bb996ae
ThisaraAriyawansha <thisara.a2001@gmail.com>
2025-07-28 11:19:48 +0530

    add project

---
+++ resources/views/about.blade.php
@@ -0,0 +1,274 @@
+<!DOCTYPE html>
+<html lang="zxx" dir="ltr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Udarata Computers | Best Computers for you</title>
+    <meta name="robots" content="index, follow" />
+    <meta name="description" content="Udarata Computers - Best Computers for you">
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.ico" />
+    <!-- CSS
+    ============================================ -->
+    <script src="assets/js/tailwind-cdn.js"></script>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="assets/css/font.awesome.css" />
+    <link rel="stylesheet" href="assets/css/pe-icon-7-stroke.css" />
+    <link rel="stylesheet" href="assets/css/animate.min.css">
+    <link rel="stylesheet" href="assets/css/swiper-bundle.min.css">
+    <link rel="stylesheet" href="assets/css/venobox.css">
+    <link rel="stylesheet" href="assets/css/jquery-ui.min.css">
+    <!-- Style CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <!-- Minify Version -->
+    <!-- <link rel="stylesheet" href="assets/css/plugins.min.css">
+    <link rel="stylesheet" href="assets/css/style.min.css"> -->
+</head>
+
+<body>
+    <div class="main-wrapper">
+    @include('layouts.nav-2')
+
+        <div class="h-[10dvh]"></div>
+        <!-- breadcrumb-area start -->
+        
+        <!-- breadcrumb-area end -->
+        <!-- About section Start -->
+        <div class="about-area py-[50px]" style="height:fit-content">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="about-wrapper text-center">
+                            <div class="about-contant">
+                                <h2 class="title">
+                                    <span>Next generation tech </span>
+                                    solutions & support
+                                </h2>
+                                <p>In our store you will find an array of hardware such as Desktop Computers, Laptops, Gaming Laptops, Branded Computer Accessories for the best price in Sri Lanka , backed up by friendly, helpful and expert staff, Giving you the very best of computers , with a focus on quality , reliability , price, brand.</p>
+                                <p class="max-[390px]:hidden">
+                                    Having built up a very solid and stable reputation for our quality products and services since 2021, Udarata Computers is the perfect choice, as we offer unrivalled expertise for all your IT Solutions and we are proud to have assisted thousands of customers since opening our first store back in 2021.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- About section End -->
+        <!-- Feature Area Srart -->
+        <div class="feature-area py-[50px]">
+            <div class="container">
+                <div class="feature-wrapper">
+                    <div class="single-feture-col mb-md-30px mb-lm-30px">
+                        <!-- single item -->
+                        <div class="single-feature">
+                            <div class="feature-icon">
+                                <img src="assets/images/icons/truck.webp" alt="">
+                            </div>
+                            <div class="feature-content">
+                                <h4 class="title">Free Shipping</h4>
+                                <span class="sub-title">Available in Kandy</span>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- single item -->
+                    <div class="single-feture-col mb-md-30px mb-lm-30px">
+                        <div class="single-feature">
+                            <div class="feature-icon">
+                                <img src="assets/images/icons/card.webp" alt="">
+                            </div>
+                            <div class="feature-content">
+                                <h4 class="title">Card Payments</h4>
+                                <span class="sub-title">12 Months Installments</span>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- single item -->
+                    <div class="single-feture-col">
+                        <div class="single-feature">
+                            <div class="feature-icon">
+                                <img src="assets/images/icons/pkg.webp" alt="">
+                            </div>
+                            <div class="feature-content">
+                                <h4 class="title">Easy Returns</h4>
+                                <span class="sub-title">Shop With Confidence</span>
+                            </div>
+                        </div>
+                        <!-- single item -->
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Feature Area End -->
+        <!-- Testimonial area start -->
+        <div class="trstimonial-area relative py-[50px] dark-blue-bg text-white border-y-1" style="background:url(assets/images/banner/gaming_5.jpg)no-repeat;background-position:center;background-size:cover;background-attachment: fixed;">
+            <div class="bg-black/50 left-0 top-0 w-full h-full absolute"></div>
+            <div class="container relative">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="section-title text-center m-0">
+                            <h2 class="title text-white">Client Feedback</h2>
+                            <p class="text-white">This is what our clients had to say about our services</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        <!-- Swiper -->
+                        <div class="swiper-container content-top slider-nav-style-1">
+                            <div class="swiper-wrapper">
+                                <div class="swiper-slide">
+                                    <div class="testi-inner">
+                                        <div class="testi-content">
+                                            <p class="text-white text-justify">I was looking for a high-performance mechanical keyboard for my gaming setup, and Udarata Computers had an amazing selection. The staff was super helpful in explaining the features, and I couldn’t be happier with my purchase. Definitely a go-to place for gamers!
+                                            </p>
+                                        </div>
+                                        <div class="testi-author">
+                                            <div class="author-image hidden">
+                                                <img class="img-responsive" src="assets/images/testimonial/1.png" alt="">
+                                            </div>
+                                            <div class="author-name">
+                                                <h4 class="name p-0 m-0">Dasun Chanaka<span class="text-white">Client</span></h4>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="swiper-slide">
+                                    <div class="testi-inner">
+                                        <div class="testi-content">
+                                            <p class="text-white text-justify">Udarata Computers made buying a laptop so easy. I had a tight budget and specific requirements for work, and they guided me to the perfect choice. The laptop has been working flawlessly, and I’m so grateful for their honest and professional advice
+                                            </p>
+                                        </div>
+                                        <div class="testi-author">
+                                            <div class="author-image hidden">
+                                                <img class="img-responsive" src="assets/images/testimonial/1.png" alt="">
+                                            </div>
+                                            <div class="author-name">
+                                                <h4 class="name p-0 m-0">Kasun Weerawardhana<span class="text-white">Client</span></h4>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="swiper-slide">
+                                    <div class="testi-inner">
+                                        <div class="testi-content">
+                                            <p class="text-white text-justify">If you’re a gamer, Udarata Computers is a paradise! I recently got a top-tier graphics card from them, and their prices were unbeatable. The team even gave me tips for installation. Highly recommend this shop to any gaming enthusiast
+                                            </p>
+                                        </div>
+                                        <div class="testi-author">
+                                            <div class="author-image hidden">
+                                                <img class="img-responsive" src="assets/images/testimonial/1.png" alt="">
+                                            </div>
+                                            <div class="author-name">
+                                                <h4 class="name p-0 m-0">Saman Ekanayaka<span class="text-white">Client</span></h4>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="swiper-slide">
+                                    <div class="testi-inner">
+                                        <div class="testi-content">
+                                            <p class="text-white text-justify">I ordered a PC upgrade from Udarata Computers, and the entire process was smooth from start to finish. The delivery was fast, the components were original, and my computer now runs faster than ever. Their expertise really shows!
+                                            </p>
+                                        </div>
+                                        <div class="testi-author">
+                                            <div class="author-image hidden">
+                                                <img class="img-responsive" src="assets/images/testimonial/1.png" alt="">
+                                            </div>
+                                            <div class="author-name">
+                                                <h4 class="name p-0 m-0">Kalindu Sachintha<span class="text-white">Client</span></h4>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="swiper-slide">
+                                    <div class="testi-inner">
+                                        <div class="testi-content">
+                                            <p class="text-white text-justify">Shopping at Udarata Computers was an excellent experience. I wanted a new monitor for both work and gaming, and the staff went above and beyond to help me find the perfect one. The quality and service exceeded my expectations!
+                                            </p>
+                                        </div>
+                                        <div class="testi-author">
+                                            <div class="author-image hidden">
+                                                <img class="img-responsive" src="assets/images/testimonial/1.png" alt="">
+                                            </div>
+                                            <div class="author-name">
+                                                <h4 class="name p-0 m-0">Kamal Fernando<span class="text-white">Client</span></h4>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Add Arrows -->
+                            <div class="swiper-buttons">
+                                <div class="swiper-button-next"></div>
+                                <div class="swiper-button-prev"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Blog area start from here -->
+        <div class="main-blog-area py-[50px] light-blue-bg">
+                <div class="container">
+                    <!-- section title start -->
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="section-title text-center mb-30px0px">
+                                <h2 class="title text-white">Latest Blogs</h2>
+                                <p class="text-white">Discover our latest articles and updates</p>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- section title start -->
+                    <div class="row">
+                        @foreach ($blogs as $blog)
+                            <div class="col-lg-6 col-sm-6 mb-xs-30px">
+                                <div class="single-blog">
+                                    <div class="blog-image">
+                                        <a href="{{ route('blog.show') . '?blog-id=' . $blog['id'] }}"><img src="{{ $blog['image'] }}" class="img-responsive w-100" alt="{{ $blog['title'] }}"></a>
+                                    </div>
+                                    <div class="lg:pl-3 max-lg:pt-3 flex flex-col gap-2 justify-center">
+                                        <div class="blog-athor-date line-height-1">
+                                            <span class="blog-date text-white"><i class="fa fa-calendar text-white" aria-hidden="true"></i> {{ $blog['date'] }}</span>
+                                            <span><a class="blog-author text-white" href="#"><i class="fa fa-user text-white" aria-hidden="true"></i> Admin</a></span>
+                                        </div>
+                                        <h5 class="blog-heading mt-2"><a class="blog-heading-link text-white" href="{{ route('blog.show') . '?blog-id=' . $blog['id'] }}">{{ $blog['title'] }}</a></h5>
+                                        <h5 class="blog-heading"><a class="blog-heading-link text-white text-sm" href="{{ route('blog.show') . '?blog-id=' . $blog['id'] }}">{{ $blog['description'] }}</a></h5>
+                                        <a href="{{ route('blog.show') . '?blog-id=' . $blog['id'] }}" class="btn btn-primary blog-btn">Read More</a>
+                                    </div>
+                                </div>
+                            </div>
+                        @endforeach
+                        @if($blogs->isEmpty())
+                            <div class="col-12 text-center">
+                                <p class="text-white">No blogs available at the moment.</p>
+                            </div>
+                        @endif
+                    </div>
+                </div>
+            </div>
+        <!-- footer -->
+        @include('layouts.footer2')
+
+    </div>
+    <!-- Global Vendor, plugins JS -->
+    <!-- JS Files
+    ============================================ -->
+    <script src="assets/js/vendor/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/vendor/jquery-3.6.0.min.js"></script>
+    <script src="assets/js/vendor/jquery-migrate-3.3.2.min.js"></script>
+    <script src="assets/js/vendor/modernizr-3.11.2.min.js"></script>
+    <script src="assets/js/plugins/jquery.countdown.min.js"></script>
+    <script src="assets/js/plugins/swiper-bundle.min.js"></script>
+    <script src="assets/js/plugins/scrollUp.js"></script>
+    <script src="assets/js/plugins/venobox.min.js"></script>
+    <script src="assets/js/plugins/jquery-ui.min.js"></script>
+    <script src="assets/js/plugins/mailchimp-ajax.js"></script>
+    <!--Main JS (Common Activation Codes)-->
+    <script src="assets/js/main.js"></script>
+</body>
+
+</html>
